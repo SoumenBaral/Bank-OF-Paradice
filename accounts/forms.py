96aac2df8065eea6a876c43e8e90ff68ae5a0347
SoumenBaral <shumen.sb@gmail.com>
2023-12-24 09:41:10 +0600

    registration from done

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,9 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django import forms 
 from django.contrib.auth.models import User
 from .constants import ACCOUNT_TYPE,GENDER_TYPE
+from .models import UserAddress,UserBankAccount
 class UserRegistrationForm(UserCreationForm):
     birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
     gender = forms.ChoiceField(choices=GENDER_TYPE)
@@ -12,5 +14,37 @@ class UserRegistrationForm(UserCreationForm):
     postal_code = forms.IntegerField()
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'account_type', 'birth_date','gender', 'postal_code', 'city','country', 'street_address']
+        fields = ['username', 'password1', 'password2', 'first_name',
+        'last_name', 'email', 'account_type', 'birth_date','gender', 'postal_code',
+        'city','country', 'street_address']
+    def save(self, commit=True) -> Any:
+        our_user =  super().save(commit = False)
+        if (commit == True ):
+            our_user.save()
+            account_type = self.cleaned_data.get('account_type')
+            gender = self.cleaned_data.get('gender')
+            birth_date = self.cleaned_data.get('birth_date')
+            country = self.cleaned_data.get('country')
+            city = self.cleaned_data.get('city')
+            street_address = self.cleaned_data.get('street_address')
+            postal_code = self.cleaned_data.get('postal_code')
+            # account_type = self.cleaned_data.get('account_type')
+            # account_type = self.cleaned_data.get('account_type')
+            # account_type = self.cleaned_data.get('account_type')
+            UserAddress.objects.create(
+                user = our_user,
+                country = country,
+                city = city, 
+                street_address = street_address,
+                postal_code = postal_code
+            )
+            UserBankAccount.objects.create(
+                user = our_user,
+                account_type = account_type ,
+                gender = gender,
+                birth_date = birth_date,
+                account_no = 120000 + our_user.id
+
+            )
+        return our_user
 
