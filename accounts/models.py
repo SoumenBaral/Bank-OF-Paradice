661d11c7209629ea9b0d7ce0bc091e6be502adff
SoumenBaral <shumen.sb@gmail.com>
2023-12-24 08:59:15 +0600

    working with admin

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class UserBankAccount(models.Model):
     account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPE)
     account_no = models.IntegerField(unique = True)
     birth_date = models.DateField(null = True,blank = True)
-    gender = models.CharField(choices=GENDER_TYPE)
+    gender = models.CharField(max_length=10,choices=GENDER_TYPE)
     initial_deposit_date = models.DateField(auto_now_add = True)
     balance = models.DecimalField(max_digits=12, decimal_places=2,null=True)
 
@@ -15,7 +15,7 @@ class UserBankAccount(models.Model):
         return str(self.account_no)
         
 class UserAddress(models.Model):
-    user = models.OneToOneField(User,related_name ="Account",on_delete=models.CASCADE)
+    user = models.OneToOneField(User,related_name ="Address",on_delete=models.CASCADE)
     country = models.CharField(max_length =100)
     city = models.CharField(max_length= 100)
     street_address = models.CharField(max_length= 100)
