e5f8d418487a6adc584f20803a78b06d5e755c34
SoumenBaral <shumen.sb@gmail.com>
2023-12-24 08:49:17 +0600

    add account models

---
+++ accounts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .constants import ACCOUNT_TYPE,GENDER_TYPE
+
+class UserBankAccount(models.Model):
+    user = models.OneToOneField(User,related_name ="Account",on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPE)
+    account_no = models.IntegerField(unique = True)
+    birth_date = models.DateField(null = True,blank = True)
+    gender = models.CharField(choices=GENDER_TYPE)
+    initial_deposit_date = models.DateField(auto_now_add = True)
+    balance = models.DecimalField(max_digits=12, decimal_places=2,null=True)
+
+    def __str__(self) -> str:
+        return str(self.account_no)
+        
+class UserAddress(models.Model):
+    user = models.OneToOneField(User,related_name ="Account",on_delete=models.CASCADE)
+    country = models.CharField(max_length =100)
+    city = models.CharField(max_length= 100)
+    street_address = models.CharField(max_length= 100)
+    postal_code = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.user.email
+
+
+
+
+
