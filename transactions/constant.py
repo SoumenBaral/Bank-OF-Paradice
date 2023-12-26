4a6f8c05e563f847a6b272d815665c02d1c24794
SoumenBaral <shumen.sb@gmail.com>
2023-12-26 22:30:41 +0600

    solve some problem

---
+++ transactions/constant.py
@@ -0,0 +1,12 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4 
+
+TRANSACTION_TYPE = (
+    ('DEPOSIT', 'deposit'),
+    ('WITHDRAWAL','withdrawal'),
+    ('LOAN','loan'),
+    ('LOAN_PAID','loan Paid'),
+
+)
